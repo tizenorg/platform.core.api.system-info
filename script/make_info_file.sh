a15699a256b5876d96578df622ab85fc4837b132
Taeyoung Kim <ty317.kim@samsung.com>
2016-03-10 12:33:37 +0900

    build info: add build id, type, variant, and release inforation
    - Examples
      = Build ID: "tizen-mobile_20160308.8_mobile-wayland-armv7l-tm1"
      = Build Type: "eng" or "user"
      = Build Variant: It can be set according to the product type
      = Build release: "20160308.8"
    
    Change-Id: I29e14b461dfe6b18b562922a3d8c03ded750b181
    Signed-off-by: Taeyoung Kim <ty317.kim@samsung.com>

--- script/make_info_file.sh
+++ script/make_info_file.sh
@@ -4,11 +4,41 @@
 
 . /etc/tizen-build.conf
 
+TYPE=eng
+VARIANT=NONE
+
+while getopts "t:v:" opt
+do
+	case "$opt" in
+	t)
+		TYPE=$OPTARG
+		;;
+	v)
+		VARIANT=$OPTARG
+		;;
+	*)
+		echo "Invalid Parameter !!"
+		echo "  Usage: make_info_file.sh [-t type] [-v variant]"
+		;;
+	esac
+done
+
+TYPE=$(echo $TYPE | tr '[:upper:]' '[:lower:]')
+
+RELEASE=$(echo $TZ_BUILD_ID | sed 's/[^0-9.]//g')
+
+BUILDID=$(sed -n '/BUILD_ID=/p' /etc/tizen-release)
+BUILDID=$(echo $BUILDID | sed 's/BUILD_ID=//g')
+
 cat <<EOF > /etc/info.ini
 [Version]
 Model=$TZ_BUILD_RELEASE_NAME;
 Build=$TZ_BUILD_ID;
+Release=$RELEASE;
 [Build]
+Type=$TYPE;
 Date=$TZ_BUILD_DATE;
 Time=$TZ_BUILD_TIME;
+Variant=$VARIANT;
+ID=$BUILDID;
 EOF
